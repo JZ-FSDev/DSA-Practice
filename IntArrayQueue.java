52f98226e6ad509fc70bc166484812415d3d5f87
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-02 22:59:56 -0600

    Create IntArrayQueue.java

---
+++ IntArrayQueue.java
@@ -0,0 +1,26 @@
+public class IntArrayQueue {
+    
+    /** The maximum size each stack can be */
+    private static final int MAX_SIZE = 100;
+
+    private int end;
+    private int[] ququeArray;
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public IntArrayQueue(){
+        ququeArray = new int[MAX_SIZE];
+        end = -1;
+    }
+
+    public void enquque(int value){
+
+    }
+
+    public int dequque(){
+
+    }
+}
