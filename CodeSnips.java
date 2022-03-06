07861ad6d5f406eab2e0206634988a20b182fad4
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-03-05 20:40:43 -0600

    Update CodeSnips.java

--- CodeSnips.java
+++ CodeSnips.java
@@ -4,7 +4,10 @@ public class CodeSnips {
      * Test the methods.
      */
     public static void main(String[] args) {
-        printLine('x', 5);
+        // printLine('x', 5);
+        int[] arr = {3,4,5,6,7};
+        System.out.println(addLess(arr, 6));
+        System.out.println(raiseIntToPower(4,4));
     }
 
     /**
@@ -20,5 +23,56 @@ public class CodeSnips {
         }
     }
 
-    
+    /**
+     * Recursive method to raise a given int n to the power of specified int k.
+     * 
+     * @param n The int to be raised to the power of int k.
+     * @param k The power to raise int n to.
+     * @return The product of n raised to k.
+     */
+    private static int raiseIntToPower(int n, int k){
+        int result = n;
+        if(k == 0){
+            result = 1;
+        }else if(k > 1){
+            result *= raiseIntToPower(n, k - 1);
+        }
+        return result;
+    }
+
+    /**
+     * Calls the private method to count the number of odd ints in the specified int array.
+     * 
+     * @param nums The array to have the number of odd ints determined.
+     * @return The count of odds in the specified array.
+     */
+    public static int countOdd(int[] nums){
+        return countOdd(nums, nums.length - 1);
+    }
+
+    private static int countOdd(int[] nums, int index){
+        int count = 0;
+        if(index >= 0){
+            if(nums[index] % 2 == 1){
+                count++;
+            }
+            count += countOdd(nums, index - 1);
+        }
+        return count;
+    }
+
+    public static int addLess(int[] nums, int key){
+        return addLess(nums, key, 0);
+    }
+
+    private static int addLess(int[] nums, int key, int index){
+        int sum = 0;
+        if(index < nums.length){
+            if(nums[index] < key){
+                sum += nums[index];
+            }
+            sum += addLess(nums, key, index + 1);
+        }
+        return sum;
+    }
 }
