ad707b3cfb558d8fb4c8e72bb5997c886314d5ff
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-16 15:11:01 -0500

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -2,13 +2,27 @@ public class Heap {
     
     private static final int DEFAULT_SIZE = 100;
     private int[] heap;
+    private int heapSize;
 
     public Heap(int size){
         heap = new int[DEFAULT_SIZE];
+        heapSize = 0;
     }
 
     public void insert(int item){
-        
+        heap[heapSize] = item;
+        int index = heapSize;
+        heapSize++;
+
+        if(index > 0){
+            while(index >= 0){
+                int oldIndex = index;
+                index = (int)Math.floor((index - 1) / 2);
+                if(item > heap[index]){
+                    swap(index, oldIndex);
+                }
+            }
+        }
     }
 
     public int deleteMax(){
@@ -17,4 +31,9 @@ public class Heap {
         return max;
     }
 
+    private void swap(int a, int b){
+        int temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+    }
 }
