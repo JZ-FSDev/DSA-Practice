56f59d13d7b04f1024dea0f439fb838f681633ac
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-16 14:57:13 -0500

    Create Heap.java

---
+++ Heap.java
@@ -0,0 +1,20 @@
+public class Heap {
+    
+    private static final int DEFAULT_SIZE = 100;
+    private int[] heap;
+
+    public Heap(int size){
+        heap = new int[DEFAULT_SIZE];
+    }
+
+    public void insert(int item){
+        
+    }
+
+    public int deleteMax(){
+        int max = Integer.MIN_VALUE;
+
+        return max;
+    }
+
+}
