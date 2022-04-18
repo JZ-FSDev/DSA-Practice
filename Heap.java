a373c3a75510ef2ee3aa1e679d33ae6ac4c25958
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-17 21:55:13 -0500

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -1,3 +1,10 @@
+/**
+ * Defines a heap with its basic operations.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class Heap {
 
     private static final int DEFAULT_SIZE = 100;
@@ -20,12 +27,23 @@ public class Heap {
         
     }
 
+    /**
+     * Creates a new heap.
+     */
     public Heap() {
         heap = new int[DEFAULT_SIZE];
         heapSize = 0;
     }
 
+    /**
+     * Inserts the new given item into this heap.
+     * 
+     * @param item The int to be added to the heap.
+     */
     public void insert(int item) {
+        if(heapSize >= heap.length){
+            increaseHeapSize();
+        }
         heap[heapSize] = item;
         int index = heapSize;
         heapSize++;
@@ -39,6 +57,11 @@ public class Heap {
         }
     }
 
+    /**
+     * Deletes the max element in this heap (the root).
+     * 
+     * @return The max element's int value.
+     */
     public int deleteMax() {
         int max = Integer.MIN_VALUE;
         if(heapSize == 1){
@@ -78,12 +101,21 @@ public class Heap {
         return max;
     }
 
+    /**
+     * Swaps the two items from the given indices a and b of this heap.
+     * 
+     * @param a The index of the first item to be swapped.
+     * @param b The index of the second item to be swapped.
+     */
     private void swap(int a, int b) {
         int temp = heap[a];
         heap[a] = heap[b];
         heap[b] = temp;
     }
 
+    /**
+     * Prints all the items in this heap.
+     */
     public void print() {
         for (int i = 0; i < heapSize; i++) {
             System.out.print(heap[i] + " ");
@@ -91,27 +123,36 @@ public class Heap {
         System.out.println();
     }
 
-    public boolean isValid(int parentIndex){
+    /**
+     * Recursive method to check if this heap is valid.
+     * 
+     * @param root The index of the root.
+     * @return True if the heap is valid.
+     */
+    public boolean isValid(int root){
         boolean valid = true;
         if(valid){
-            if(2 * parentIndex + 1 < heapSize){
-                if(heap[parentIndex] < heap[2 * parentIndex + 1]){
+            if(2 * root + 1 < heapSize){
+                if(heap[root] < heap[2 * root + 1]){
                     valid = false;
                 }else{
-                    valid = isValid(2 * parentIndex + 1); // left child
+                    valid = isValid(2 * root + 1); // left child
                 }
             }
-            if(2 * parentIndex + 2 < heapSize){
-                if(heap[parentIndex] < heap[2 * parentIndex + 2]){
+            if(2 * root + 2 < heapSize){
+                if(heap[root] < heap[2 * root + 2]){
                     valid = false;
                 }else{
-                    valid = isValid(2 * parentIndex + 2); // right child
+                    valid = isValid(2 * root + 2); // right child
                 }
             }
         }
         return valid;
     }
 
+    /**
+     * Increases the heap to 1.5x its previous size.
+     */
     private void increaseHeapSize(){
         int[] temp = new int[(int)(heapSize * 1.5)];
         for(int i = 0; i < heapSize; i++){
