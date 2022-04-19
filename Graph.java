ce598c8f6a3dd23266cc902062a77a60949878e0
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-04-18 21:24:36 -0500

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -19,4 +19,17 @@ public class Graph {
             }
         }
     }
+    
+
+    public static void main(String[] args) {
+
+        Graph graph = new Graph(5);
+        
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 2);
+
+        graph.print();
+    }    
 }
