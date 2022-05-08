d88b57539d10057b13bd9779d933cea7ac171540
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-05-07 22:47:52 -0500

    Update CBasics.c

--- CBasics.c
+++ CBasics.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 struct person{
     int age;
-    char name[];
+    char name[100];
 };
 
 int equalPerson(struct person person1, struct person person2){
@@ -10,7 +11,7 @@ int equalPerson(struct person person1, struct person person2){
 }
 
 void printPerson(struct person person){
-    printf("Name: %d  Age: %d", person.name, person.age);
+    printf("Name: %s  Age: %d\n", person.name, person.age);
 }
 
 int sumEven(int max)
@@ -45,15 +46,41 @@ void arrayCopy(int arrayFrom[], int arrayTo[], int start, int end, int length)
     }
 }
 
+
+
 int main(int numParms, char *parms[])
 {
     // int ans = sumEven(100);
     // printf("The sum of evens is %d\n", ans);
-    int first[] = {1, 2, 3};
-    int second[] = {4, 5, 6};
+    // int first[] = {1, 2, 3};
+    // int second[] = {4, 5, 6};
 
-    printArray(3, first);
-    printArray(3, second);
+    // printArray(3, first);
+    // printArray(3, second);
+
+    int count = 100;
+    struct person persons[count];
+    for(int i = 0; i < count; i++){
+        persons[i].age = i;
+        char character[] = "moo";
+        char temp[] = "cow";
+        strcpy(persons[i].name, strncat(character, temp, 3));
+    }
+    for(int i = 0; i < count; i++){
+        printPerson(persons[i]);
+    }
+
+    // struct person person1;
+    // struct person person2;
+    // char personName1[] = "Jason";
+    
+
+    // person1.age = 25;
+    // strcpy(person1.name, "Jason");
+    // person2.age = 13;
+    // strcpy(person2.name, "Lisa");
+    // printPerson(person1);
+    // printPerson(person2);
 }
 
 
