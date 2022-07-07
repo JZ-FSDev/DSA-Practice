22c4fd03941c5801c174b8f2813a4464cc6ec50c
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-07-06 22:23:57 -0500

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -14,3 +14,15 @@ void insertAscended(int num);
 void validateList();
 int removeLastItem();
 void printList();
+
+void insertAscended(int thisNum){
+    Node *prev = top;
+    Node *curr = NULL;
+    Node *toLink = malloc(sizeof(Node));
+
+    if(toLink != NULL){
+        toLink -> num = thisNum;
+        toLink -> next = NULL;
+        if(prev == NULL){
+            top = toLink;
+        }else{
